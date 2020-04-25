2f46b25162c38f0b07f2bc5015ff216b12d44218
root <1046762075@qq.com>
2020-04-25 22:12:35 +0800

    gmall

---
+++ src/main/java/com/firenay/gmall/GmAllApplication.java
@@ -0,0 +1,13 @@
+package com.firenay.gmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmAllApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/firenay/gmall/entity/UserInfo.java
@@ -0,0 +1,10 @@
+package com.firenay.gmall.entity;
+
+/**
+ * <p>Title: UserInfo</p>
+ * Description：
+ * date：2020/4/25 22:10
+ */
+public class UserInfo {
+
+}
